062b5b42d0eb2bfad94b0ac576f8b8e4bfcd4889
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-03 18:18:31 +0530

    feat: create flutter project for todo App

---
+++ android/app/src/main/kotlin/com/mahendranmurugan/todoapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mahendranmurugan.todoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:todoapp/views/home_page.dart';
+
+void main() {
+  runApp(const myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+    );
+  }
+}
